2d90a0ae44be6312481ef520d69c6458aa43abed
Carlos André <n_ao_eu@hotmail.com>
2015-12-14 09:12:43 -0300

    Update admin.py

--- sisgebo_app/admin.py
+++ sisgebo_app/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from models import *
 admin.site.register(fazenda)
 admin.site.register(Definicao_sistema_e_rebanho_inicial)
+admin.site.register(pastagem)
+admin.site.register(Compra_e_venda)
