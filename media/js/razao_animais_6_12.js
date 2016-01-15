0e98fecb3feee38c07f18aed6ade950f140462c3
eucandre <eucandre@gmail.com>
2016-01-15 00:32:13 -0200

    terminando pastagem

---
+++ media/js/razao_animais_6_12.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Carlos on 15/01/2016.
+ */
+
+var razao_animais_6_12 = document.getElementById("id_razao_area_piquete_categoria_piquete_animais6_12");
+var animais_6_12 = document.getElementById("id_animais6_12");
+var peso_animais_6_12 = document.getElementById("id_peso_animais6_12_kg");
+
+var tempo_pastejo= document.getElementById("id_tempo_pastejo");
+var tempo_descanso= document.getElementById("id_tempo_descanso");
+
+
+function efetuaCalculo(){
+  //Cálculo simples de potência só pra ilustrar
+  razao_animais_6_12.innerHTML = ((parseInt(animais_6_12.value)*(parseInt(peso_animais_6_12.value)))/450)/1.5/(parseInt(parseInt(tempo_descanso.value)/parseInt(tempo_pastejo.value))+1).value;;
+}
+
+animais_6_12.addEventListener("keyup", efetuaCalculo);
+peso_animais_6_12.addEventListener("keyup", efetuaCalculo);
+tempo_descanso.addEventListener("keyup", efetuaCalculo);
+tempo_pastejo.addEventListener("keyup", efetuaCalculo);
---
+++ media/js/razao_piquetes_femeas.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Carlos on 14/01/2016.
+ */
+
+/**
+ * Created by Carlos on 14/01/2016.
+ */
+
+var razao_area_piquete_categoria_piquete_femeas = document.getElementById("id_razao_area_piquete_categoria_piquete_femeas");
+var n_femeas = document.getElementById("id_n_femeas");
+var peso_femea = document.getElementById("id_peso_femeas_kg");
+var cap_sup = document.getElementById("id_cap_suporte");
+
+
+var tempo_pastejo= document.getElementById("id_tempo_pastejo");
+var tempo_descanso= document.getElementById("id_tempo_descanso");
+
+
+function efetuaCalculo(){
+  //Cálculo simples de potência só pra ilustrar
+  razao_area_piquete_categoria_piquete_femeas.innerHTML = ((parseInt(n_femeas.value)*parseInt(peso_femea.value))/450)/((1.5))/
+      (parseInt(parseInt(tempo_descanso.value)/parseInt(tempo_pastejo.value))+1).value;
+
+}
+
+tempo_descanso.addEventListener("keyup", efetuaCalculo);
+tempo_pastejo.addEventListener("keyup", efetuaCalculo);
+n_femeas.addEventListener("keyup", efetuaCalculo);
+peso_femea.addEventListener("keyup", efetuaCalculo);
+cap_sup.addEventListener("keyup", efetuaCalculo);
+
