cd044ef48a6fd6a6381ca1239840c3ddd1a0ca46
eucandre <eucandre@gmail.com>
2016-02-02 10:34:06 -0200

    editado

---
+++ media/1Fluxo_caixa/total_entradas.js
@@ -0,0 +1,13 @@
+
+var total_entradas = document.getElementById("");
+var RECEITAS_FAZENDA_janeiro = document.getElementById("");
+var RECEITA_EMPRÉSTIMO_janeiro= document.getElementById("");
+
+function total_ENTRADAS_janeiro(){
+    total_entradas.innerHTML = parseInt(RECEITAS_FAZENDA.value)+parseInt(RECEITA_EMPRÉSTIMO.value);
+}
+total_ENTRADAS_janeiro();
+
+total_entradas.addEventListener("keyup",total_ENTRADAS_janeiro);
+RECEITAS_FAZENDA_janeiro.addEventListener("keyup",total_ENTRADAS_janeiro);
+RECEITA_EMPRÉSTIMO_janeiro.addEventListener("keyup",total_ENTRADAS_janeiro);
---
+++ media/1Fluxo_caixa/total_fluxo_elemento.js
@@ -0,0 +1,53 @@
+
+var total_elemento_receita_fazenda = document.getElementById("");
+var total_elemento_receita_fazenda_jan = document.getElementById("");
+var total_elemento_receita_fazenda_fev = document.getElementById("");
+var total_elemento_receita_fazenda_mar = document.getElementById("");
+var total_elemento_receita_fazenda_abr = document.getElementById("");
+var total_elemento_receita_fazenda_mai = document.getElementById("");
+var total_elemento_receita_fazenda_jun = document.getElementById("");
+var total_elemento_receita_fazenda_jul = document.getElementById("");
+var total_elemento_receita_fazenda_ago = document.getElementById("");
+var total_elemento_receita_fazenda_set = document.getElementById("");
+var total_elemento_receita_fazenda_out = document.getElementById("");
+var total_elemento_receita_fazenda_nov = document.getElementById("");
+var total_elemento_receita_fazenda_dez = document.getElementById("");
+
+function total_Receita_fazenda(){
+    total_elemento_receita_fazenda.innerHTML =
+        parseInt(total_elemento_receita_fazenda_jan.value)+
+        parseInt(total_elemento_receita_fazenda_fev.value)+
+        parseInt(total_elemento_receita_fazenda_mar.value)+
+        parseInt(total_elemento_receita_fazenda_abr.value)+
+        parseInt(total_elemento_receita_fazenda_mai.value)+
+        parseInt(total_elemento_receita_fazenda_jun.value)+
+        parseInt(total_elemento_receita_fazenda_jul.value)+
+        parseInt(total_elemento_receita_fazenda_ago.value)+
+        parseInt(total_elemento_receita_fazenda_set.value)+
+        parseInt(total_elemento_receita_fazenda_out.value)+
+        parseInt(total_elemento_receita_fazenda_nov.value)+
+        parseInt(total_elemento_receita_fazenda_dez.value);
+}
+
+total_elemento_receita_fazenda_jan.addEventListener("keyup",total_Receita_fazenda);
+total_elemento_receita_fazenda_fev.addEventListener("keyup",total_Receita_fazenda);
+total_elemento_receita_fazenda_mar.addEventListener("keyup",total_Receita_fazenda);
+total_elemento_receita_fazenda_abr.addEventListener("keyup",total_Receita_fazenda);
+total_elemento_receita_fazenda_mai.addEventListener("keyup",total_Receita_fazenda);
+total_elemento_receita_fazenda_jun.addEventListener("keyup",total_Receita_fazenda);
+total_elemento_receita_fazenda_jul.addEventListener("keyup",total_Receita_fazenda);
+total_elemento_receita_fazenda_ago.addEventListener("keyup",total_Receita_fazenda);
+total_elemento_receita_fazenda_set.addEventListener("keyup",total_Receita_fazenda);
+total_elemento_receita_fazenda_out.addEventListener("keyup",total_Receita_fazenda);
+total_elemento_receita_fazenda_nov.addEventListener("keyup",total_Receita_fazenda);
+total_elemento_receita_fazenda_dez.addEventListener("keyup",total_Receita_fazenda);
+
+
+
+
+
+
+
+
+
+
